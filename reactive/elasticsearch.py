fcdb079fa007390b75f09e618befb9251a389ad1
Charles Butler <chuck@dasroot.net>
2016-02-01 20:53:45 +0100

    Initial Commit

---
+++ reactive/elasticsearch.py
@@ -0,0 +1,9 @@
+from charms.reactive import when, when_not
+from charms import ansible
+
+from charms.reactive.bus import get_states
+
+@when('ansible.available')
+@when_not('elasticsearch.available')
+def do_something():
+    ansible.apply_playbook('playbook.yaml', tags=get_states()) 
